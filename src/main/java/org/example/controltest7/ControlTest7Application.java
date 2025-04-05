af3ad3db334671ba1ea632ce815fc18b0821dbb0
kanat <esenalievkanat.90@gmail.com>
2025-04-05 12:07:57 +0600

    Автоматически созданы при созданий проекта

---
+++ src/main/java/org/example/controltest7/ControlTest7Application.java
@@ -0,0 +1,11 @@
+package org.example.controltest7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlTest7Application {
+    public static void main(String[] args) {
+        SpringApplication.run(ControlTest7Application.class, args);
+    }
+}
---
+++ src/test/java/org/example/controltest7/ControlTest7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.controltest7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlTest7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
