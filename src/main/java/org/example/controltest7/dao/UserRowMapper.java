df130865e26db64d2bc27862027cb02013a4395d
kanat <esenalievkanat.90@gmail.com>
2025-04-05 12:08:23 +0600

    Создал класс UserRowMapper, который реализует интерфейс RowMapper<User>. Этот класс отвечает за преобразование результатов SQL-запросов в объекты User. Метод mapRow извлекает данные из ResultSet и заполняет ими поля объекта User

---
+++ src/main/java/org/example/controltest7/dao/UserRowMapper.java
@@ -0,0 +1,19 @@
+package org.example.controltest7.dao;
+
+import org.example.controltest7.model.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper implements RowMapper<User> {
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        User user = new User();
+        user.setId(rs.getLong("id"));
+        user.setUsername(rs.getString("username"));
+        user.setPassword(rs.getString("password"));
+        user.setEnabled(rs.getBoolean("enabled"));
+        return user;
+    }
+}
